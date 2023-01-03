be43eac1fc703d9dc430674000da6256436f72a1
BrynjarGeir <brynjargeir1@gmail.com>
2023-01-03 15:05:48 +0000

    Finished day 17 with a lot of help for part 2 from hugseverycat.

--- 2022/Day_17/Python/part2.py
+++ 2022/Day_17/Python/part2.py
@@ -1,20 +1,18 @@
-# Using hugseverycat for cycle part 2
+# Using hugseverycat for cycle part 2 (using his github)
 
-from collections import defaultdict
 from time import time
 
 #file = '../data/test'
 file = '../data/input'
 
 with open(file) as f:
-    inp = [1 if c == '>' else -1 for c in f.readline()]
+    jet_pattern = [1 if c == '>' else -1 for c in f.readline()]
 
 height = 0
-above = 4
+above = 3
 rocks_to_drop = 1000000000000
 num_rocks = 5
 states = {}
-occupied = defaultdict(lambda: False)
 columns = {
     0: set(),
     1: set(),
@@ -24,10 +22,8 @@ columns = {
     5: set(),
     6: set()
 }
-for point in ((0,0), (1,0), (2,0), (3,0), (4,0), (5,0), (6,0)):
-    occupied[point] = True
-n = len(inp)
-index = 0
+num_jets = len(jet_pattern)
+jet = 0
 
 
 rock1 = [(2,0), (3,0), (4,0), (5,0)]
@@ -38,24 +34,18 @@ rock5 = [(2,0), (2,1), (3,0), (3,1)]
 
 rocks = [rock1, rock2, rock3, rock4, rock5]
 
-def pushRock(rock, direction, occupied):
-    tmp = rock.copy()
-    for i in range(len(rock)):
-        if occupied[(rock[i][0]+direction, rock[i][1])]:
+def pushRock(rock, direction, columns):
+    tmp = [(tx+direction, ty) for tx,ty in rock]
+    for tx, ty in tmp:
+        if tx == -1 or tx== 7 or ty in columns[tx]:
             return rock, False
-        elif rock[i][0]+direction == -1 or rock[i][0]+direction == 7:
-            return rock, True
-        else:
-            tmp[i] = (rock[i][0]+direction, rock[i][1])
     return tmp, True
 
-def fallRock(rock, occupied):
-    tmp = rock.copy()
-    for i in range(len(rock)):
-        if occupied[(rock[i][0], rock[i][1]-1)]:
+def fallRock(rock, columns):
+    tmp = [(tx, ty-1) for tx,ty in rock]
+    for tx,ty in tmp:
+        if ty in columns[tx] or ty < 0:
             return rock, False
-        else:
-            tmp[i] = (rock[i][0], rock[i][1]-1)
     return tmp, True
 
 def update_pos(columns, rock):
@@ -70,67 +60,69 @@ def get_height(columns):
         if len(columns[col]):
             m = max(columns[col])
             h = max(m, h)
-    return h+1
+    return h + 1
 
-def find_cycle(occupied, columns, index, rock, rocks_to_drop, drop):
+def newRock(rock, rocks, height, above):
+    nRock = [(tx,ty+height+above) for tx,ty in rocks[rock]]
+    return nRock
+
+def find_cycle(columns, jet, rock, rocks_to_drop, height, drop, states):
     max_cols = []
     for c in columns:
-        if columns[c]:
+        if len(columns[c]):
             max_cols.append(max(columns[c]))
         else:
             max_cols.append(-1)
     min_col = min(max_cols)
     relative_cols = [mc - min_col for mc in max_cols]
-    state = relative_cols.extend([rock, index])
+    state = relative_cols.extend([rock, jet])
     state = tuple(relative_cols)
 
     if state in states:
-        height_gain_in_cycle = max(max_cols) - states[state]['height']
         rocks_in_cycle = drop - states[state]['rock']
+        height_gain_in_cycle = height - states[state]['height']
         remaining_rocks = rocks_to_drop - drop
         cycles_remaining = remaining_rocks // rocks_in_cycle
         rock_remainder = remaining_rocks % rocks_in_cycle
         height_increase = height_gain_in_cycle * cycles_remaining
-        return True, height_increase
+        return True, height_increase, rocks_to_drop - rock_remainder, states
     else:
         states[state] = {'rock': drop, 'height': height}
-        return False, None
+        return False, None, None, states
 
 
-def runSimulation(rocks_to_drop, num_rocks, height, above, index, occupied, columns):
-    for drop in range(rocks_to_drop):
+def runSimulation(rocks_to_drop, num_rocks, height, above, jet, columns, cycle_found = False, drop = 0, states = {}):
+    while drop < rocks_to_drop:
 
         rock = drop % num_rocks
-        cr = rocks[rock]
+        cr = newRock(rock, rocks, height, above)
         cnt = True
-
-        cr = [(r[0],r[1]+height+above) for r in cr]
+        tmp_drop = None
 
         while cnt:
-            index %= n
-            cr, cnt = pushRock(cr, inp[index], occupied)
+            cr, cnt = pushRock(cr, jet_pattern[jet], columns)
 
-            cr, cnt = fallRock(cr, occupied)
+            cr, cnt = fallRock(cr, columns)
 
-            index += 1
+            jet = (jet + 1) % num_jets
 
         columns = update_pos(columns, cr)
         height = get_height(columns)
-
-
-        for point in cr:
-            occupied[point] = True
-            columns[point[0]].add(point[1])
         
-        cycle_found, height_increase = find_cycle(occupied, columns, index, rock, rocks_to_drop, drop)
+        if not cycle_found:
+            cycle_found, height_increase, tmp_drop, states = find_cycle(columns, jet, rock, rocks_to_drop, height, drop, states)
+        
+        if tmp_drop:
+            drop = tmp_drop
+        drop += 1
 
-        if cycle_found:
-            return height, height_increase  
 
+    if cycle_found:
+        return height, height_increase
     return height, None
 
 start = time()
-height, height_increase = runSimulation(rocks_to_drop, num_rocks, height, above, index, occupied, columns)
+height, height_increase = runSimulation(rocks_to_drop, num_rocks, height, above, jet, columns)
 end = time()
 
 print('The height we stopped at is ', height)
